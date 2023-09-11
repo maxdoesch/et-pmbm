dc47ecee6aa45dbff7b9c96b6dfc9d9b462af176
Maximilian Dösch <doesch.maximilian@gmail.com>
2023-09-11 10:06:19 +0200

    changed min_partitioning_radius

--- include/tracker/constants.h
+++ include/tracker/constants.h
@@ -7,7 +7,7 @@ namespace tracker
     double constexpr p_detection = 0.99;
     double constexpr clutter_rate = 0.001;
     double constexpr parent_detection_radius = 3;
-    double constexpr min_partition_radius = 0.5;
+    double constexpr min_partition_radius = 0.25;
     double constexpr max_partition_radius = 2;
     double constexpr partition_radius_step = 0.25;
     int constexpr min_cluster_size = 5;
